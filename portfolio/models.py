f3e183544fdb9d5d7829c24f68c30f2c458956a9
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-04-17 15:50:44 -0700

    Added an email form to the contact page

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class EmailForm(models.Model):
+    sender_name = models.CharField(max_length=80)
+    sender_email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return f"From {self.sender_name} ({self.sender_email}): {self.message}"
